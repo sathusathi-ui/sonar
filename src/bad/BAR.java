54292911fe3a3e3638b9d550c6d1e8f795e758b3
sathu <sathu@example.com>
2025-12-25 20:03:57 +0530

    third commit

--- src/bad/BAR.java
+++ src/bad/BAR.java
@@ -4,13 +4,11 @@ public class BAR {
 	
 
 	    public static void main(String[] args) {
-
-	        String name = null;
-	        System.out.println(name.length());   // BUG: NullPointerException
-
-	        int a = 10;
-	        int b = 0;
-	        int c = a / b;                        // BUG: Division by zero
+	    	int a = 10;
+	        int b = 5;
+	        int c = a / b; 
+	       
+	        System.out.println(c);   
 
 	    }
 	}
